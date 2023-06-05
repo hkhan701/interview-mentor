f4267e9971492ae4750544156e5cbaf86cedfbb8
hkhan701 <96705217+hkhan701@users.noreply.github.com>
2023-06-04 19:28:54 -0500

    added typing effect and text area for questions

--- js/app.js
+++ js/app.js
@@ -18,65 +18,96 @@ const pulseAnimationOff = () => {
     }); 
 }
 
-const beginInterview = document.querySelector('.begin-interview');
-
-beginInterview.addEventListener('click', () => {
-    textToSpeech("Hello, welcome to InterviewMentor. Please enter the questions you would like to practice! You may add up to 5 questions below.");
-});
-
-
-// Mute button functionality
-
-const muteFunctionality = () => {
-const muteButton = document.querySelector('.pulse');
-    muteButton.addEventListener('click', () => {
-
-        if (muteButton.classList.contains('pulse-mute')) {
-
-            muteButton.classList.remove('pulse-mute');
+function typeText(element, text) {
+    element.innerHTML = "";
+    return new Promise((resolve) => {
+        let index = 0;
+    
+        let interval = setInterval(() => {
+          if (index < text.length) {
+            pulseAnimationOn();
+            element.innerHTML += text.charAt(index);
+            index++;
+          } else {
             pulseAnimationOff();
-
-        } else {
-
-            muteButton.classList.add('pulse-mute');
-            window.speechSynthesis.cancel();  
-            pulseAnimationOff();
-            
-        }
+            clearInterval(interval);
+            setTimeout(resolve, 2000);
+          }
+    
+    }, 50);
     });
 }
 
-// TEXT TO SPEECH function
-
-const textToSpeech = (text) => {
-
-
-    const muteButton = document.querySelector('.pulse');
-
-    if (muteButton.classList.contains('pulse-mute')) { // if mute button is on, do not speak
-        return;
-    }
-
-    pulseAnimationOn(); // pulse animation on
-
-    // We output the text to the textarea
+const startUp =  async () => {
     const aiResponse = document.getElementById('response'); 
-    aiResponse.value = text;
-
-    // We speak the text
-    const utterance = new SpeechSynthesisUtterance(text);  
-    window.speechSynthesis.speak(utterance);
-
-    utterance.onend = function (event) { // pulse animation off once speech ends
-        pulseAnimationOff();
-    };
+    await typeText(aiResponse, "Hello, welcome to InterviewMentor. Please enter the questions you would like to practice! You may add up to 5 questions below.");
+    await typeText(aiResponse, "When you are ready to begin, click the begin interview button.");
 }
 
-textToSpeech("Hello, welcome to InterviewMentor. Please enter the questions you would like to practice! You may add up to 5 questions below.");
-muteFunctionality();
+startUp();
+
+const beginInterview = document.querySelector('.begin-interview');
+
+beginInterview.addEventListener('click', () => {
+    
+});
+
+// Mute button functionality
+
+// const muteFunctionality = () => {
+//     const muteButton = document.querySelector('.pulse');
+//     muteButton.addEventListener('click', () => {
+
+//         if (muteButton.classList.contains('pulse-mute')) {
+//             muteButton.classList.remove('pulse-mute');
+//             pulseAnimationOff();
+
+//         } else {
+
+//             muteButton.classList.add('pulse-mute');
+//             window.speechSynthesis.cancel();  
+//             pulseAnimationOff();
+
+//         }
+//     });
+// }
+
+
+// const aiResponse = document.getElementById('response'); 
+// typeText(aiResponse, "Hi, my name is Hassan.");
 
 
 
 
+// TEXT TO SPEECH function
 
+// const textToSpeech = (text) => {
 
+//     const muteButton = document.querySelector('.pulse');
+//     window.speechSynthesis.cancel();
+
+//     // We output the text to the textarea
+//     const aiResponse = document.getElementById('response'); 
+//     typeText(aiResponse, text);
+
+//     // if (aiResponse.value.trim() === "") {
+//     //     return; // Exit the function if there is no text
+//     // }
+
+//     // We speak the text
+//     const utterance = new SpeechSynthesisUtterance(text);  
+//     if (!muteButton.classList.contains('pulse-mute')) { // if mute button is off,speak     
+//         window.speechSynthesis.volume = 1; // 0 to 1
+//     } 
+
+//     window.speechSynthesis.speak(utterance); 
+//     pulseAnimationOn();
+
+//       utterance.onend = function (event) {
+//         pulseAnimationOff();
+//       };
+
+//       return new Promise((resolve) => {
+//         utterance.onend = resolve;
+//       });
+// }
